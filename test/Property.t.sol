0fa498c4973204382591432e05061e21af9bebcf
TheJoker971 <jojo.blsegor@gmail.com>
2024-10-30 16:55:26 +0100

    Initialize project, first commit

--- test/Property.t.sol
+++ test/Property.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Property} from "../src/Property.sol";
 
-contract CounterTest is Test {
+contract PropertyTest is Test {
     Property public property;
 
     function setUp() public {
