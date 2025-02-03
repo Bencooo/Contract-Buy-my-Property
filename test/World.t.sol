bf0831409b20a660f0834942c27fcf555c241b84
TheJoker971 <jojo.blsegor@gmail.com>
2025-02-03 14:35:51 +0100

    test

--- test/World.t.sol
+++ test/World.t.sol
@@ -7,16 +7,39 @@ import {World} from "../src/World.sol";
 contract WorldTest is Test {
     World public world;
 
+    struct Position {
+        uint256 x;
+        uint256 y;
+    }
+
     function setUp() public {
         world = new World("Test", "TEST");
+    }
+
+    function testConstructor() public view {
+        assertEq("Test",world.name());
+        assertEq("TEST",world.symbol());
     }
 
     function testMint() public {
         world.mint(1, 1);
         vm.expectRevert(abi.encodeWithSelector(World.POSITION_ALREADY_USED.selector, 1, 1));
         world.mint(1, 1);
-        assertEq(world.getPositionOf(0).x, 1);
-        assertEq(world.getPositionOf(0).y, 1);
+        Position pos = world.getPositionOf(0);
+        assertEq(pos.x, 1);
+        assertEq(pos.y, 1);
         assertEq(world.getNumberOfProperties(), 1);
+        assertEq(world.ownerOf(0),msg.sender);
     }
+
+    function testPositionAvailable() public {
+        world.min(1,1);
+        assertEq(false,world.isPositionAvailable(1,1));
+    }
+
+    function testBalanceOf() public {
+        world.mint(1,1);
+        assertEq(1,world.balanceOf(msg.sender));
+    }
+
 }
